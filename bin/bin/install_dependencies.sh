00f467d81c8a9a97238d18e13709488b85bdc4b4
sduenasg <sara85gon@gmail.com>
2019-01-09 17:41:37 +0100

    updated install dependencies script

--- bin/bin/install_dependencies.sh
+++ bin/bin/install_dependencies.sh
@@ -1,7 +1,24 @@
 #!/bin/bash
-echo -e "\n---- using apt ---"
 
-sudo apt install zsh
+haveProg(){
+	[-x "$(which $1)"]
+}
+
+install_apt(){
+	sudo apt-get install $1
+}
+
+install_pacman(){
+	sudo pacman -S $1
+}
+
+if haveProg apt-get ; then install_apt zsh
+elif haveProg pacman ; then install_pacman zsh
+else
+	echo 'No supported package manager found'
+	exit 2
+fi
+
 chsh -s $(which zsh)
 
 packages=(git figlet cowsay powerline powerline-fonts python3 python3-pip fortune imagemagick)
@@ -9,7 +26,12 @@ packages=(git figlet cowsay powerline powerline-fonts python3 python3-pip fortun
 for pkg in ${packages[@]}
 do
 	echo -e "\nInstalling \033[31m$pkg\033[0m from repo:"
-	sudo apt install $pkg
+	if haveProg apt-get ; then install_apt $pkg
+	elif haveProg pacman ; then install_pacman $pkg
+	else
+		echo 'no supported package manager found'
+		exit 2
+	fi
 done
 
 sudo pip3 install pywal
